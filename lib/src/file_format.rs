4bf76f95c4487de607504f5239960ebab2235cc4
kuviman <kuviman@gmail.com>
2023-10-12 13:20:47 +0400

    versioning

--- lib/src/file_format.rs
+++ lib/src/file_format.rs
@@ -2,25 +2,89 @@ use super::*;
 
 use std::path::PathBuf;
 
-#[derive(Serialize, Deserialize)]
-pub enum Image {
-    Load(PathBuf),
-    Embed { size: vec2<usize>, data: Vec<u8> },
+pub mod versions {
+    use super::*;
+
+    pub mod v0 {
+        use super::*;
+
+        pub const HEADER: &str = "PogPaint";
+
+        #[derive(Serialize, Deserialize)]
+        pub enum Image {
+            Load(PathBuf),
+            Embed { size: vec2<usize>, data: Vec<u8> },
+        }
+
+        #[derive(Serialize, Deserialize)]
+        pub struct Plane {
+            pub image: Option<Image>,
+            pub offset: vec2<i32>,
+            pub transform: mat4<f32>,
+        }
+
+        #[derive(Serialize, Deserialize)]
+        pub struct Pp {
+            pub planes: Vec<Plane>,
+        }
+    }
+
+    pub mod v1 {
+        use super::*;
+        pub const VERSION: u8 = 1;
+
+        #[derive(Serialize, Deserialize)]
+        pub enum Image {
+            Load(PathBuf),
+            Embed { size: vec2<usize>, data: Vec<u8> },
+        }
+
+        #[derive(Serialize, Deserialize)]
+        pub struct Plane {
+            pub image: Option<Image>,
+            pub offset: vec2<i32>,
+            pub transform: mat4<f32>,
+        }
+
+        #[derive(Serialize, Deserialize)]
+        pub struct Pp {
+            pub planes: Vec<Plane>,
+        }
+
+        impl From<v0::Image> for Image {
+            fn from(old: v0::Image) -> Self {
+                match old {
+                    v0::Image::Load(path) => Self::Load(path),
+                    v0::Image::Embed { size, data } => Self::Embed { size, data },
+                }
+            }
+        }
+
+        impl From<v0::Plane> for Plane {
+            fn from(old: v0::Plane) -> Self {
+                Self {
+                    image: old.image.map(Into::into),
+                    offset: old.offset,
+                    transform: old.transform,
+                }
+            }
+        }
+
+        impl From<v0::Pp> for Pp {
+            fn from(old: v0::Pp) -> Self {
+                Self {
+                    planes: old.planes.into_iter().map(Into::into).collect(),
+                }
+            }
+        }
+    }
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Plane {
-    image: Option<Image>,
-    offset: vec2<i32>,
-    transform: mat4<f32>,
-}
+use versions::v1 as current_version;
 
-#[derive(Serialize, Deserialize)]
-pub struct Pp {
-    pub planes: Vec<Plane>,
-}
+use current_version::{Image, Plane, Pp};
 
-const HEADER: &str = "PogPaint";
+const HEADER: &str = "VersionedPogPaint";
 
 impl Model {
     pub fn save(&self, mut writer: impl std::io::Write) -> std::io::Result<()> {
@@ -45,6 +109,7 @@ impl Model {
                 .collect(),
         };
         writer.write_all(HEADER.as_bytes())?;
+        writer.write_all(&[current_version::VERSION])?;
         let mut encoder = flate2::write::GzEncoder::new(writer, flate2::Compression::best());
         encoder.write_all(&bincode::serialize(&pp).unwrap())?;
         encoder.finish()?;
@@ -57,16 +122,40 @@ impl Model {
     ) -> std::io::Result<Self> {
         let mut buf = Vec::new();
         let mut reader = std::pin::pin!(reader);
-        {
-            let mut header = vec![0; HEADER.len()];
-            reader.read_exact(&mut header).await?;
-            assert_eq!(header, HEADER.as_bytes());
-        }
+        let version = {
+            let mut first_byte = vec![0];
+            reader.read_exact(&mut first_byte).await?;
+            let first_byte = first_byte[0];
+            if first_byte == versions::v0::HEADER.as_bytes()[0] {
+                let mut header = vec![0; versions::v0::HEADER.len()];
+                header[0] = first_byte;
+                reader.read_exact(&mut header[1..]).await?;
+                assert_eq!(header, versions::v0::HEADER.as_bytes());
+                0
+            } else {
+                let mut header = vec![0; HEADER.len()];
+                header[0] = first_byte;
+                reader.read_exact(&mut header[1..]).await?;
+                assert_eq!(header, HEADER.as_bytes());
+                let mut version_byte = vec![0];
+                reader.read_exact(&mut version_byte).await?;
+                version_byte[0]
+            }
+        };
+        println!("{version}");
         reader.read_to_end(&mut buf).await.unwrap();
         let mut decoder = flate2::read::GzDecoder::new(buf.as_slice());
         let mut buf = Vec::new();
         decoder.read_to_end(&mut buf).unwrap();
-        let pp: Pp = bincode::deserialize(&buf).unwrap();
+        let pp: current_version::Pp = match version {
+            0 => {
+                let v0: versions::v0::Pp = bincode::deserialize(&buf).unwrap();
+                v0.into()
+            }
+            versions::v1::VERSION => bincode::deserialize(&buf).unwrap(),
+            _ => todo!("unknown version {version}"),
+        };
+
         Ok(Self {
             ugli: asset_manager.ugli().clone(),
             planes: stream::iter(pp.planes.into_iter())
